c80c19e75c8628e982d8e793b1b29436e270c0f9
Jony <jntorres2014@gmail.com>
2022-12-29 19:01:23 -0300

    subiendo base

--- sge/settings.py
+++ sge/settings.py
@@ -83,6 +83,12 @@ WSGI_APPLICATION = 'sge.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'sge.db'),
+    # }
+
     'default': dj_database_url.config(
             default= 'postgresql://postgres:postgres@localhost/postgres',
             conn_max_age=600
