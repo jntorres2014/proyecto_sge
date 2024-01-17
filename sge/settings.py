41628460aca31e95466c52636cd878b0304abb8c
jntorres2014 <jntorres2014@gmail.com>
2024-01-17 13:44:50 -0300

    base2

--- sge/settings.py
+++ sge/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+# import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,15 +91,15 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 
 DATABASES = {
     
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'sge.db'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'sge.db'),
+    }
 
-    'default': dj_database_url.config(
-            default= 'postgresql://postgres:postgres@localhost/postgres',
-            conn_max_age=600
-        )
+    # 'default': dj_database_url.config(
+    #         default= 'postgresql://postgres:postgres@localhost/postgres',
+    #         conn_max_age=600
+    #     )
     
 }
 
