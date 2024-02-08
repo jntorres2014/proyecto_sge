6d9e2b44107bc536d5e2bef5e43d57391f064388
jnntorres2014 <jntorres2014@gmail.com>
2024-02-08 18:39:54 -0300

    agrego reporte pdf

--- Core/views.py
+++ Core/views.py
@@ -1,10 +1,11 @@
-import datetime
+from datetime import datetime
 from io import BytesIO
 import string
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
+from Clases.models import Inasistencias
 from Core import forms
 from django.urls import reverse
 from Core.resource import LocalidadResource
@@ -569,9 +570,11 @@ def asignar_estudiante_a_aula(request,id_anio):
                                                                  'cantAlumnos': cantidadInscriptos,
                                                                  'cantDivisiones': cantidadDivisiones})
 
+
 def exportar_historial_pdf(request, estudiante_id):
     estudiante = Estudiante.objects.get(pk=estudiante_id)
     inscripciones = inscripcionEstudianteCiclo.objects.filter(estudiante=estudiante).order_by('fecha')
+    faltas = Inasistencias.objects.filter(estudiante=estudiante).order_by('dia')
     
     # Crear un buffer de bytes en memoria para el PDF
     buffer = BytesIO()
@@ -587,24 +590,32 @@ def exportar_historial_pdf(request, estudiante_id):
     elements.append(Paragraph("Historial del Estudiante", styles['Title']))
     elements.append(Spacer(1, 12))
     elements.append(Paragraph(f"Estudiante: {estudiante.Nombre} {estudiante.Apellido}", styles['Normal']))
-    elements.append(Paragraph(f"Fecha de alta: {estudiante.fechaInscripcion}", styles['Normal']))
+    elements.append(Paragraph(f"Fecha de alta: {(estudiante.fechaInscripcion).strftime('%d-%m-%Y')}", styles['Normal']))
     elements.append(Spacer(1, 12))
 
     # Datos del historial
-    data = [["Plan de Estudio", "Ciclo",'Año', "Fecha de Inscripción"]]
+    data = [["Plan de Estudio", "Ciclo", 'Año', "Fecha de Inscripción"]]
     for inscripcion in inscripciones:
-        data.append([inscripcion.ciclo.plan.anio, inscripcion.ciclo,inscripcion.anio, datetime.strptime(inscripcion.fecha).strftime("%d %B %Y")])
-
+        data.append([
+            inscripcion.ciclo.plan.anio,
+            inscripcion.ciclo,
+            inscripcion.anio, 
+            inscripcion.fecha.strftime("%d-%m-%Y")
+        ])
+    
     # Crear tabla con los datos del historial
     table = Table(data)
-    table.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.black),
-                               ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-                               ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-                               ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-                               ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-                               ('BACKGROUND', (0, 1), (-1, -1), colors.white),
-                               ('GRID', (0, 0), (-1, -1), 1, colors.black)]))
-        # Establecer ancho de las columnas
+    table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.black),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ]))
+    
+    # Establecer ancho de las columnas
     table._argW[0] = 2.5 * inch  # Ancho de la primera columna
     table._argW[1] = 2 * inch    # Ancho de la segunda columna
     table._argW[2] = 1.5 * inch  # Ancho de la tercera columna
@@ -616,6 +627,40 @@ def exportar_historial_pdf(request, estudiante_id):
     ]))
     
     elements.append(table)
+
+    # Datos de inasistencias
+    inasistencias = [['Fecha', 'Justificada?']]
+
+    for falta in faltas:
+        justificada = 'SI' if falta.justificacion else 'NO'
+        inasistencias.append([
+            falta.dia.strftime("%d-%m-%Y"),  # Ajusta el formato según sea necesario
+            justificada])
+
+    # Crear tabla con los datos de inasistencias
+    table_inasistencias = Table(inasistencias)
+    table_inasistencias.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.black),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ]))
+    
+    # Establecer ancho de las columnas
+    table_inasistencias._argW[0] = 2 * inch  # Ancho de la primera columna
+    table_inasistencias._argW[1] = 2 * inch  # Ancho de la segunda columna
+    
+    # Establecer espacio entre filas
+    table_inasistencias.setStyle(TableStyle([
+        ('SPACEBEFORE', (0,0), (-1,-1), 12),
+        ('SPACEAFTER', (0,0), (-1,-1), 12)
+    ]))
+    
+    elements.append(Paragraph("Inasistencias:", styles['Title']))
+    elements.append(table_inasistencias)
 
     # Construir el PDF
     doc.build(elements)
